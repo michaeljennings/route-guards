2b75bcf55dad849631cc0f3f9941ea8e16249855
Michael Jennings <michaeljennings@gmlconsulting.co.uk>
2020-05-19 12:32:51 +0100

    Add route guard macro

--- src/RouteGuardServiceProvider.php
+++ src/RouteGuardServiceProvider.php
@@ -2,9 +2,20 @@
 
 namespace MichaelJennings\RouteGuards;
 
+use Illuminate\Routing\Route;
 use Illuminate\Support\ServiceProvider;
 
 class RouteGuardServiceProvider extends ServiceProvider
 {
-
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Route::macro('guard', function (string $guard, string $binding = null) {
+            $this->action['guard'] = $binding ? [$binding => $guard] : $guard;
+        });
+    }
 }
